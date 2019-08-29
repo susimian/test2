9c1ae514b715a3d5735886fa8cb5da04a3fa7f9b
su_jiayan <281954081@qq.com>
2019-08-29 15:02:12 +0800

    t2

--- src/main/java/com/example/sendmail/demo/controller/HelloController.java
+++ src/main/java/com/example/sendmail/demo/controller/HelloController.java
@@ -2,7 +2,7 @@ package com.example.sendmail.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+// 控制器
 @RestController
 public class HelloController {
     @GetMapping("/admin/hello")
